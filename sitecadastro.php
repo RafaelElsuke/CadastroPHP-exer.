1b6b9fafec264c9d16b1a24c8eeba1f9d0dbff49
Rafael rodrigues <120226602+RafaelElsuke@users.noreply.github.com>
2024-07-09 10:40:05 -0300

    Add files via upload

--- sitecadastro.php
+++ sitecadastro.php
@@ -9,6 +9,28 @@
     <title>tela de cadastro</title>
 </head>
 <style>
+    nav {
+        text-align: center;
+        font-family: 'Times New Roman', Times, serif;
+        font-size: 20px;
+        margin: 20px;
+        background-color: #382c27;
+        padding: 10px;
+
+    }
+
+    .teste {
+        text-decoration: none;
+        color: white;
+        padding: 10px;
+    }
+
+    h1 {
+        font-size: 40px;
+        text-align: center;
+
+    }
+
     .bt {
         margin-top: 10px;
         padding: 16px 42px;
@@ -38,10 +60,12 @@
     }
 
     body {
-        background-color: black;
+        background-color: #f7530c;
+        height: 100vh;
+        overflow: hidden;
     }
 
-    div {
+    #ts {
         background-color: palegreen;
         display: flex;
         flex-direction: column;
@@ -95,37 +119,70 @@
             }
         }
     }
+
+    :root {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+    }
+
+    #footer {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 2rem;
+        background: purple;
+        color: white;
+        font-weight: 500;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 0.2rem;  
+    }
 </style>
 
 <body>
+    <header>
+
+        <h1 class="he">Projeto fake Banco</h1>
+
+        <nav>
+            <a class="teste" href="index.html">PIX</a>
+            <a class="teste" href="about.html">SOBRE</a>
+            <a class="teste" href="contact.html">CONTATOS</a>
+        </nav>
+
+    </header>
     <?php
     if (isset($_SESSION['msg'])) {
         echo $_SESSION['msg'];
         unset($_SESSION['msg']);
     }
     ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
+    <div  id="ts"class="container">
+        <div id="ts" class="row">
+            <div id="ts" class="col-md-12">
+                <div id="ts"class="card">
                     <div class="card-header">Crie seu Cadastro</div>
-                    <div class="card-body">
+                    <div id="ts" class="card-body">
                         <form action="cadastro02.php" method="post">
-                            <div class="form-group">
+                            <div id="ts" class="form-group">
                                 <label for="nome">Nome:</label>
-                                <input type="text" name="nome" class="form-control"
-                                    placeholder="Digite o nome" required>
+                                <input type="text" name="nome" class="form-control" placeholder="Digite o nome"
+                                    required>
                                 <label for="sobrenome">Sobrenome:</label>
                                 <input type="text" name="sobrenome" class="form-control"
                                     placeholder="Digite o sobrenome" required>
                                 <label for="CPF">CPF:</label>
-                                <input type="number" name="CPF" class="form-control"
-                                    placeholder="Digite seu CPF" required>
+                                <input type="number" name="CPF" class="form-control" placeholder="Digite seu CPF"
+                                    required>
                                 <label for="email">E-mail:</label>
-                                <input type="text" name="email" class="form-control"
-                                    placeholder="Digite seu email" required>
+                                <input type="text" name="email" class="form-control" placeholder="Digite seu email"
+                                    required>
                                 <label for="email">Telefone:</label>
-                                <input type="number" name="telefone"  class="form-control"
+                                <input type="number" name="telefone" class="form-control"
                                     placeholder="Digite seu Telefone" required>
                                 <input type="submit" name="botão" id="botão" class="form-control" value="cadastrar">
                                 <a href="ar.php"><input type='button' class="bt" value="LISTAR" </a>
@@ -137,6 +194,10 @@
         </div>
     </div>
     </div>
+
+
+
+    <div id="footer">&copy;<span id="year"> </span><span> Your Company Name. All rights reserved.</span></div>
 
 
 
